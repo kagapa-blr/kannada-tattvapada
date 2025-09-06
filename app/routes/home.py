104ee531db6f4f99e76d948d8dde3edc60c5f7a0
Ravikumar-Pawar <ravi.ravipawar17@gmail.com>
2025-09-06 20:44:21 +0530

    UI fonts updated

--- app/routes/home.py
+++ app/routes/home.py
@@ -2,16 +2,15 @@
 Home route for displaying Tatvapada records in the main view.
 """
 
-from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import Blueprint, render_template
 
 from app.utils.auth_decorator import login_required
-from app.utils.logger import setup_logger
-
 
 # Create a Blueprint for home
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route('/')
+@login_required
 def home_page():
     return render_template("index.html")
 
